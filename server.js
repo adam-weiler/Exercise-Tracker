1e169b0b0ac63aa4b9988e09693821243de68657
Glitch (dog-comic) <none>
2019-01-29 01:34:14 +0000

    🙀🐝 Updated with Glitch - Story 5 complete.

--- server.js
+++ server.js
@@ -64,6 +64,7 @@ const exerciseSchema = new mongoose.Schema({
 
 var Exercise = mongoose.model('Exercise', userSchema);
 
+console.log("Starting up the app again!");
 
 // your first API endpoint... 
 app.get("/api/hello", function(req, res) {
@@ -73,7 +74,7 @@ app.get("/api/hello", function(req, res) {
 });
 
 
-app.post("/api/exercise/new-user", function(req, res) {
+app.post("/api/exercise/new-user", function(req, res) { //User filled out the "Create a New User" form.
 	let user_name = req.body.username; //Gets the user_name from the body of the page.
 
 	if (!user_name) { //User input is blank.
@@ -82,21 +83,22 @@ app.post("/api/exercise/new-user", function(req, res) {
 
 	findUserByName(user_name, function(err, data) { //Checks if input user_name is in database already.
 		if (err) {
+			console.log("findUserByName error: ", err);
 			res.json(err);
 		}
 
 		if (data != null) { //If not null then user_name is in database already.
-			console.log('Username already taken.');
+			console.log('Username already taken: ', user_name);
 			res.json('Username already taken.');
 		} else { //Else is null so user_name is not in database.
 			console.log('This username is not taken.');
 
 			createAndSaveUser(user_name, function(err, data) { //Creates and saves new user to database.
 				if (err) {
-					console.log("Error saving new username to database.");
+					console.log("createAndSaveUser error: ", err);
 					res.json(err);
 				}
-				console.log("New username saved to database.");
+				console.log("New username saved to database: ", user_name);
 				res.json({
 					"username": user_name,
 					"_id": data
@@ -107,8 +109,8 @@ app.post("/api/exercise/new-user", function(req, res) {
 });
 
 
-//Outputs a list of all Users.
-app.get("/api/exercise/users", function(req, res) {
+
+app.get("/api/exercise/users", function(req, res) { //Outputs a list of all Users.
 	// User.find({}, function(err, users) {
 	// 	res.send(users.reduce(function(userMap, item) {
 	// 		userMap[item.id] = item;
@@ -118,13 +120,12 @@ app.get("/api/exercise/users", function(req, res) {
 
 	findAllUsers(function(err, users) { //Finds all users in the database.
 		if (err) {
-			console.log("Error: ", err);
-			res.json("Error: ", err);
-		} else {
-			console.log("Success: ", users);
-			res.json(users);
-			//res.json(users.map(function(item) { return item["username", "_id"]; }));
+			console.log("findAllUsers error: ", err);
+			res.json(err);
 		}
+		console.log("Found all users in database: ", users);
+		res.json(users);
+		//res.json(users.map(function(item) { return item["username", "_id"]; }));
 	})
 
 	//   User.find({}, function(err, users) {
@@ -141,8 +142,7 @@ app.get("/api/exercise/users", function(req, res) {
 });
 
 
-//Adds a new Exercise Log.
-app.post("/api/exercise/add", function(req, res) {
+app.post("/api/exercise/add", function(req, res) { //User filled out the "Add Exercises" form.
 	let userInput = {
 		user_id: req.body.userId,
 		desc: req.body.description,
@@ -150,10 +150,10 @@ app.post("/api/exercise/add", function(req, res) {
 		date: req.body.date
 	}; //Gets the user_id, description, and duration from the body of the page.
 
-	console.log("userInput", userInput);
-	console.log("userInput.user_id", userInput.user_id);
-	console.log("userInput.desc", userInput.desc);
-	console.log("userInput.dur", userInput.dur);
+	console.log("userInput: ", userInput);
+	// console.log("userInput.user_id", userInput.user_id);
+	// console.log("userInput.desc", userInput.desc);
+	// console.log("userInput.dur", userInput.dur);
 
 	if (!userInput.desc) { //Input description is blank.
 		console.log("No Description entered.");
@@ -161,15 +161,15 @@ app.post("/api/exercise/add", function(req, res) {
 	} else if (!userInput.dur) { //Input duration is blank.
 		console.log("No Duration entered.");
 		res.json('Path `duration` is required.');
-	} else { //User entered description and duration.
+	} else { //User entered both description and duration.
 		//if (!user_id) { //User input is blank.
 		//	res.json('Path `username` is required.');
 		//}
 
-		findUserById(userInput.user_id, function(err, oldDoc) { //Checks if user input is in database already as user_id.
+		findUserById(userInput.user_id, function(err, oldDoc) { //Checks if input user_id a valid user.
 			if (err) {
-				console.log("Error: ", err);
-				res.json("Error: ", err);
+				console.log("findUserById error: ", err);
+				res.json(err);
 			}
 
 			if (oldDoc == null) { //The user_id is not in database.
@@ -178,17 +178,23 @@ app.post("/api/exercise/add", function(req, res) {
 			} else { //The user_id is in database.
 				console.log("That user exists: ", oldDoc);
 
-				if (!userInput.date) { //User did not enter a date.
-					userInput.date = new Date(); //Will use current time.
-					userInput.date = userInput.date.toDateString();
+				if (!userInput.date || !isNaN(userInput.date)) { //User did not enter a date.
+					//userInput.date = new Date(); //Will use current time.
+					//userInput.date = userInput.date.toDateString();
+					userInput.date = new Date().getTime();
+
 					console.log("User did not input date. Use current: ", userInput.date);
-				} else if (!isNaN(userInput.date)) { //User input is a number.
-					userInput.date = userInput.date * 1000;
-					userInput.date = new Date(userInput.date).toDateString();
-					console.log("User input a number: ", userInput.date);
+
+					//} else if (!isNaN(userInput.date)) { //User input is a number.
+					//userInput.date = userInput.date * 1000;
+					//userInput.date = new Date(userInput.date).toDateString();
+
+					//userInput.date = new Date(userInput.date).getTime();
+					//console.log("User input a number: ", userInput.date);
 				} else { //User entered yyyy-mm-dd or a string.
 					//userInput.date = new Date(userInput.date).toDateString(); //Converts user_input to a UTC timestamp.
-					userInput.date = new Date(userInput.date).toDateString(); //Converts user_input to a UTC timestamp.
+					//userInput.date = new Date(userInput.date).toDateString(); //Converts user_input to a UTC timestamp.
+					userInput.date = new Date(userInput.date).getTime();
 					console.log("User entered yyyy-mm-dd or a string: " + userInput.date);
 				}
 
@@ -220,14 +226,16 @@ app.post("/api/exercise/add", function(req, res) {
 						console.log("findAndUpdate error: " + err);
 						res.json(err);
 
-					} else {
+					} else { //Saved new exercise log to database.
 						console.log("findAndUpdate success: " + err);
 						res.json({
 							"username": data.username,
 							"description": userInput.desc,
 							"duration": userInput.dur,
 							"_id": userInput.user_id,
-							"date": userInput.date
+							//"date": userInput.date
+
+							"date": new Date(userInput.date).toDateString()
 						})
 					}
 				});
@@ -250,13 +258,18 @@ app.post("/api/exercise/add", function(req, res) {
 });
 
 
-//Outputs a list of one user's Exercise logs.
-app.get('/api/exercise/:param1', function(req, res) {
+app.get('/api/exercise/log', function(req, res) { //Outputs a list of one user's Exercise logs.
 	//let user_input = req.params.input;
+	//let from_range = req.query.from;
+	//let to_range = req.query.to;
+	//Gets the user_id and optional fields from URL.
 	let user_id = req.query.userId;
+	let records_limit = req.query.limit;
+	let from_range = new Date(req.query.from).getTime();
+	let to_range = new Date(req.query.to).getTime();
+	console.log("\n user_id: " + user_id + ", limit: " + records_limit + ", from: " + from_range + ", to: " + to_range);
 
 	//http://www.google.com/api/exercise/userId?qs1=you&qs2=tube
-
 	//   findUserById(userInput, function(err, oldDoc) { //Checks if user input is in database already as user_id.
 	// 			if (err) {
 	// 				console.log("Error: ", err);
@@ -269,23 +282,21 @@ app.get('/api/exercise/:param1', function(req, res) {
 	// 			} else { //Else not null then user_id is in database.
 	//       }
 	//   });
-
 	// if (!user_id) { //User input is blank.
 	// 	res.send("unknown userId: " + user_id);
 	// }
 
-
-	findUserById(user_id, function(err, oldDoc) { //Checks if user input is in database already as user_id.
+	findUserById(user_id, function(err, userData) { //Checks if user input is in database already as user_id.
 		if (err) {
-			console.log("Database error: ", err);
-			res.json("Database error: ", err);
+			console.log("findUserById error: ", err);
+			res.json(err);
 		}
 
-		if (oldDoc == null) { //The user_id is not in database.
-			console.log("There is not user with that name in the database.");
+		if (userData == null) { //The user_id does not exist.
+			console.log("There is not user with that name in the database: ", user_id);
 			res.send('unknown userId');
 		} else { //The user_id is in database.
-			console.log("That user exists: ", oldDoc);
+			console.log("Data before data processing: ", userData);
 
 			// countLogsById(user_id, function(err, count) { //Checks if user input is in database already as user_id.
 			// if (err) {
@@ -293,14 +304,71 @@ app.get('/api/exercise/:param1', function(req, res) {
 			// 	res.json("Database error: ", err);
 			// }
 			//console.log("My count is: ", count);
+			// var items = list.slice(0, size).map(i => {
+			//   return <myview item={i} key={i.id} />
+			// }
+			if (from_range) { //User entered optional field of from_range.
+				console.log("from: " + from_range);
+				//   userData.log = userData.log.filter(function(x) {
+				//     return x > 120 && x < 130;
+				// });
+				//         userData.log = userData.log.filter((item: any) => {
+				//     return item.date.getTime() >= fromDate.getTime() &&
+				//            item.date.getTime() <= toDate.getTime();
+				// });
 
-			res.json({
-				_id: oldDoc._id,
-				username: oldDoc.username,
-				count: oldDoc.log.length,
-				log: oldDoc.log
-			});
+				userData.log = userData.log.filter(function(item) { //Returns records that are above the from_range.
+					//return (item.date >= from_range);
+					//return (item.duration >= 700);
+
+					//return (item.date >= from_range);
+					return (item.date >= from_range);
+				});
+			}
+
+			//console.log ("Data after from_range: ", userData);
+
+			if (to_range) { //User entered optional field of to_range.
+				console.log("to: " + to_range);
+
+				userData.log = userData.log.filter(function(item) { //Returns records that are below the to_range.
+					//return (item.date >= from_range);
+					//return (item.duration >= 700);
+					return (item.date <= to_range);
+				});
+
+			}
+			//userData.log.date = "puppies";
+			// userData.log = userData.log.map(function (item) {
+			//   //return (new Date(item.date).toDateString());
+			//   //return item.date = 7;
+			// });
+			// userData.log.forEach(function(item) {
+			//   userData.log(item) = 7;
+			// });
+			//    userData.log.forEach((item[date]) => {
+			//   return 7
 			// })
+			//console.log("Data after to range: ", userData);
+
+			userData.log = userData.log.filter(function(item) { //Converts all dates to readable date-format
+				return (item.date = new Date(item.date).toDateString());
+			});
+
+			if (records_limit) { //User entered optional field of records_limit.
+				console.log("limit: " + records_limit);
+				userData.log = userData.log.slice(0, records_limit); //Returns records within the limit.
+			}
+
+			//console.log ("Data after records_limit: ", userData);
+			console.log("Data after data processing: ", userData);
+
+      res.json({
+				_id: userData._id,
+				username: userData.username,
+				count: userData.log.length,
+				log: userData.log
+			});
 		}
 	})
 })
@@ -368,13 +436,13 @@ var findUserById = function(userId, done) { //Check the database if userId exist
 			console.log("Database error: ", err);
 			done(err);
 		}
-		console.log("Search was completed: ", data);
+		//console.log("Search was completed: ", data);
 		done(null, data);
 	})
 };
 
 
-var findAndUpdate = function(userInput, updatedLog, done) {
+var findAndUpdate = function(userInput, updatedLog, done) { //Updates one user's exercise log.
 	User.findOneAndUpdate({
 		_id: userInput.user_id
 	}, {
